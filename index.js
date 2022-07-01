8f01837c50fa94fcd05200b69bbc34f77cc9aa43
Juan Pablo Blum <jpblum890@gmail.com>
2022-07-01 19:47:33 +0100

    2

--- index.js
+++ index.js
@@ -3,32 +3,42 @@ var app = express();
 import fetch from "node-fetch";
 app.set('view engine', "hbs");
 app.set('port', 3000);
+import bodyParser from "body-parser";
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', async function (req, res, next) {
+  res.render('main');
+});
 
-app.get('/user', async function (req, res, next) {
-  const response = await fetch("https://api.github.com/users/jpblum95");
-  let data = await response.json();
-  console.log(data);
-  res.render('main')
+app.get('/user/:username', async function (req, res, next) {
+  let usuarioBuscador = req.params.username;
+  const response = await fetch(`https://api.github.com/users/${usuarioBuscador}`);
+  const data = await response.json();
+  const data2 = {
+    login: data.login,
+    avatar: data.avatar_url,
+    nombre: data.name,
+    repositorio: data.repos_url
+  };
+  console.log(data2);
+  res.render('main2', data2);
+  return res.json({ data: data2 });
   //entrando a localhost:3000/user aparece data en consola.
 });
 
-//funcion tomar usuario del buscador
-/*var form = document.getElementById('userBuscado');
-
-form.addEventListener('submit', function(event){
-  event.preventDefault(); //prevents the form from autosubmitting
-
-  let username = document.getElementById('usuario').value 
-
-  console.log(username)
+/*
+router.get('/:name', function(req, res, next) {
+  var name = req.params.name;
+  var user = data.filter(u => u.name == name );
+  return res.json({ message: 'Users Show', data: user });
 });
 */
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
 // npm install: express, node-fetch, express-handlebars --save, nodemon,
